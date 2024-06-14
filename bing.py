e315b1a39a151454ce03c75086181f4b033dadab
color7921 <132988693+color7921@users.noreply.github.com>
2024-06-14 20:02:24 +0900

    bing 랭체인
    bing 랭체인

---
+++ bing.py
@@ -0,0 +1,33 @@
+#!pip install langchain
+#!pip install openai
+#!pip install google-search-results
+import os
+os.environ["OPENAI_API_KEY"] = ""
+os.environ["SERPAPI_API_KEY"] = ""
+
+from langchain.agents import initialize_agent
+from langchain.chat_models import ChatOpenAI
+from langchain import LLMMathChain, SerpAPIWrapper
+from langchain.agents import Tool
+
+search = SerpAPIWrapper()
+params = {
+    "engine": "bing",
+    "gl": "us",
+    "hl": "en"
+}
+search = SerpAPIWrapper(params=params)
+
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo') 
+
+tools = [
+    Tool(
+        name = "Search",
+        func=search.run,
+        description="검색하는 API"
+    )
+]
+
+agent = initialize_agent(tools, llm, verbose=True)
+result = agent.invoke("한국의 대통령이 누구야?")
+print(result['output'])
